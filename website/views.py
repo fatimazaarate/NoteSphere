1ee0f9b11656b83542bdbc25185442d878f5cada
fatimazaarate <fatima.zaarate@gmail.com>
2024-04-24 14:31:12 +0100

    landing

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-  return "home"
+  return render_template("home.html")
